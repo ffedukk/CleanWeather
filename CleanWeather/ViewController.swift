fa01f0449cff545704d3b42d9e172da1924af928
Fedor <semfed2393@yandex.ru>
2020-09-06 15:18:26 +0300

    Initial Commit

---
+++ CleanWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanWeather
+//
+//  Created by 18592232 on 06.09.2020.
+//  Copyright © 2020 18592232. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
